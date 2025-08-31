e68127598d2618fac803a4ffbf4f59bd52d26e81
SixtenGedda <sixten.gedda@gmail.com>
2025-08-31 22:22:17 +0200

    Add styling and develop structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,27 @@
 <script>
 </script>
 
-<main class="mt-4 flex flex-col items-center bg-gray-100">
-	<h1 class="text-6xl font-bold">Sixten Gedda</h1>
-	<p class="mt-4 text-xl text-gray-600">some of my current projects exploring ux design</p>
+<main class="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+	<div class="max-w-lg bg-gray-300">
+		<h1 class="text-center text-6xl font-bold">Sixten Gedda</h1>
+		<p class="mt-4 pt-10 text-xl text-gray-600">
+			Hi! I'm a fullstack developer and welcome to my current projects exploring ux design.
+		</p>
+	</div>
 </main>
 
-<section class="flex justify-center bg-gray-200 p-5">
+<section class="flex min-h-screen justify-center bg-gradient-to-br from-gray-200 to-gray-700 p-60">
 	<a
 		href="/projects/bus-app"
-		class="flex max-w-sm transform overflow-hidden rounded border p-5 transition-transform duration-300 hover:scale-105"
+		class="flex max-w-4xl transform items-center overflow-hidden rounded-xl bg-blue-800/30 p-10 transition-transform duration-300 hover:scale-105"
 	>
-		<img src="/images/bus-app-preview.png" alt="bus app preview" class="h-auto w-24" />
+		<img src="/images/bus-app-preview.png" alt="bus app preview" class="max-h-48 w-24 rounded" />
 
 		<div class="flex flex-col p-5">
-			<h2 class="text-2xl font-bold">A reimagined public transit application</h2>
-			<p class="mt-2 text-gray-600">
-				Making travel effortless. Designing around the idea of seamless travel.
+			<h2 class="text-2xl font-bold text-white">A reimagined public transit application</h2>
+			<p class="mt-2 text-gray-300">
+				Making travel effortless. Designing around the idea of seamless travel while creating an
+				accessible and fast experience.
 			</p>
 		</div>
 	</a>
