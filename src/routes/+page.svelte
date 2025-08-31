c690b6725353ae35e8975a98c7a41c502596e9a9
SixtenGedda <sixten.gedda@gmail.com>
2025-08-31 20:26:06 +0200

    Add basic home page structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<main class="mt-4 flex flex-col items-center bg-gray-100">
+	<h1 class="text-6xl font-bold">Sixten Gedda</h1>
+	<p class="mt-4 text-xl text-gray-600">some of my current projects exploring ux design</p>
+</main>
+
+<section class="flex justify-center bg-gray-200 p-5">
+	<a
+		href="/projects/bus-app"
+		class="flex max-w-sm transform overflow-hidden rounded border p-5 transition-transform duration-300 hover:scale-105"
+	>
+		<img src="/images/bus-app-preview.png" alt="bus app preview" class="h-auto w-24" />
+
+		<div class="flex flex-col p-5">
+			<h2 class="text-2xl font-bold">A reimagined public transit application</h2>
+			<p class="mt-2 text-gray-600">
+				Making travel effortless. Designing around the idea of seamless travel.
+			</p>
+		</div>
+	</a>
+</section>
